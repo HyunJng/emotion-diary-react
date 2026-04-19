f7fd16c7153350f6b9c3147f35ee78d810b0b513
HyunJng <khjung1654@gmail.com>
2026-04-20 03:24:01 +0900

    feat: 공통 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div>
+      <Header 
+        title={"타이틀"}
+        leftChild={<Button text={"<"} type={"DEFAULT"} onClick={() => {}}/>}
+        rightChild={<Button text={">"} type={"DEFAULT"} onClick={() => {}}/>}
+      />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
