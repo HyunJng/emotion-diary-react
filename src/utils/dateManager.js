be74e18b9d1eacc1e4a3ec340fe18453640d39c4
HyunJng <khjung1654@gmail.com>
2026-04-21 15:29:35 +0900

    fix: 한자리 일자 선택 안되는 오류 수정

--- src/utils/dateManager.js
+++ src/utils/dateManager.js
@@ -9,7 +9,7 @@ export const getStringDate = (targetDate) => {
         month = `0${month}`;
     }
     if(day < 10) {
-        day = `0${date}`;
+        day = `0${day}`;
     }
     return `${year}-${month}-${day}`;
 }
